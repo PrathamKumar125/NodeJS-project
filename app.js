28093d2b031f612c9a09226d77e5fdcbabc49049
Pratham Kumar <115283906+PrathamKumar125@users.noreply.github.com>
2023-06-28 13:34:38 +0530

    app.js

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ const fs = require("fs");
 const app = express();
 const port = 80;
 
-// EXPRESS SPECIFIC STUFF
-app.use('/static', express.static('static')) // For serving static files
+
+app.use('/static', express.static('static')) 
 app.use(express.urlencoded())
 
-// PUG SPECIFIC STUFF
-app.set('view engine', 'pug') // Set the template engine as pug
-app.set('views', path.join(__dirname, 'views')) // Set the views directory
+app.set('view engine', 'pug') 
+app.set('views', path.join(__dirname, 'views')) 
  
-// ENDPOINTS
 app.get('/', (req, res)=>{
-    const con = "This is the best content on the internet so far so use it wisely"
-    const params = {'title': 'PubG is the best game', "content": con}
+    const con = "This is the Best GYM Website"
+    const params = {'title': 'Best GYM Website', "content": con}
     res.status(200).render('index.pug', params);
 })
 
@@ -33,8 +31,6 @@ app.post('/', (req, res)=>{
 
 })
 
-
-// START THE SERVER
 app.listen(port, ()=>{
     console.log(`The application started successfully on port ${port}`);
 });
